e9145ba40dc4aeb2fbc7bfc9c337bd33bd9d4e32
Charles <charlesikilo@gmail.com>
2022-12-05 13:02:28 +0100

    form update

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,10 @@ function Form(){
     return(
         <div className="  mt-20 lg:mt-20 lg:mb-20  lg:m-auto lg:w-1/2   ">
             <p className="text-pur text-3xl text-center mb-2 ">Contact Me</p>
-        <form className=" lg:m-auto  mb-20 lg:w-1/2 ">
-            <input type='text' className="my-2 w-full h-10" placeholder="Name"/> <br/>
-            <input type='email' className="my-2 w-full h-10"  placeholder="Email"/><br/>
-            <input type='textarea' className="my-2 w-full h-20"  placeholder="Your Message"/><br/>
+        <form className=" lg:m-auto  mb-20 lg:w-1/2 " action="https://formsubmit.co/charlesikilo@gmail.com" method="POST">
+            <input type='text' className="my-2 w-full h-10" placeholder="Name" name="name" required/> <br/>
+            <input type='email' className="my-2 w-full h-10"  placeholder="Email" name="email" required/><br/>
+            <input type='textarea' className="my-2 w-full h-20"  placeholder="Your Message" name="message" required/><br/>
             <button className="bg-pur w-full h-10">Send Message</button>
 
         </form>
